502486c7a2890bbb08c82ac2730f8c595f8b487c
Ian Hancock <northroadian@gmail.com>
2022-08-16 13:03:27 -0500

    scalar types

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// Scalar types which are: String, Int, Float, Boolean
+
 const typeDefs = gql`
 type Query{
-    hello: String
+    hello: String,
+    numberOfShoes: Int,
+    price: Float,
+    isCool: Boolean
 }
 `
 const resolvers = {
     Query: {
         hello: () => {
             return "Hello World"
+        },
+        numberOfShoes: () => {
+            return 84
+        },
+        price: () => {
+            return 84.99
+        },
+        isCool: () => {
+            return false
         }
     }
 }
@@ -21,4 +35,4 @@ server.listen().then(({ url }) => {
     console.log("Server is ready at" + url)
 })
 
-// using "node index" in the command line spins thi server up 
+// using "node index" in the command line spins this server up 
