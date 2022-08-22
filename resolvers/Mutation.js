2aa3d175c661d1c713ecb961721c179c9dd9a5e1
Ian Hancock <northroadian@gmail.com>
2022-08-22 14:17:30 -0500

    delete category

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,7 +1,7 @@
 const { v4: uuid } = require("uuid")
 // pulling in the categories data the array of categories we made reusable 
 exports.Mutation = {
-    addCategory: (parents, { input }, { categories }) => {
+    addCategory: (parents, { input }, { db }) => {
         const { name } = input
         const newCategory = {
             id: uuid(),
@@ -10,12 +10,12 @@ exports.Mutation = {
         }
         // this will take a random generated id and the name either selected or typed in
         // and push it to the end of the categories array. 
-        categories.push(newCategory)
+        db.categories.push(newCategory)
         // the new category must be returned from this function basically
         return newCategory
     },
     // I am extracting the input and the products array from context. 
-    addProduct: (parent, { input }, { products }) => {
+    addProduct: (parent, { input }, { db }) => {
         const {
             name,
             image,
@@ -33,11 +33,11 @@ exports.Mutation = {
             quantity,
             categoryId
         }
-        products.push(newProduct)
+        db.products.push(newProduct)
         return newProduct
     },
 
-    addReview: (parent, { input }, { review }) => {
+    addReview: (parent, { input }, { db }) => {
         const { date, title, comment, rating } = input
         const newReview = {
             id: uuid(),
@@ -47,7 +47,22 @@ exports.Mutation = {
             rating,
             productId
         }
-        review.push(newReview)
+        db.reviews.push(newReview)
         return newReview
+    },
+    deleteCategory: (parent, { id }, { db }) => {
+        db.categories === db.categories.filter(category => category.id !== id)
+        db.products = db.products.map((product) => {
+
+            if (product.categoryId === id)return {
+                    ...product,
+                    categoryId: null
+                }
+                else {
+                return product
+            }
+        })
+        return true
+
     }
 }
